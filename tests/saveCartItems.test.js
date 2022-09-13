848664045bc090f64f826c684638cc92484a56f6
Renanbfreitas <renanbf1992@outlook.com>
2022-09-12 21:26:19 -0300

    Adicionando item 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,15 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('3.1 Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+    saveCartItems('carItem');
+    expect(localStorage.setItem)
+    .toBeCalled();
+  });
+
+  it('3.2 Teste se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo carItens o segundo parâmetro', () => {
+    saveCartItems('carItem');
+    expect(localStorage.setItem)
+    .toBeCalledWith('cartItems', 'carItem');
+  });
 });
